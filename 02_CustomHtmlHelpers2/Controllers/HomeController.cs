55c2cd937428e40de236e448a07db5a8e03460dc
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-02-01 18:59:06 +0300

    hjfgf

---
+++ 02_CustomHtmlHelpers2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _02_CustomHtmlHelpers2.Extensions;
+
+namespace _02_CustomHtmlHelpers2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string GetText()
+        {
+            string text = "çalışkan";
+            return text.ClearTurkishCharacters();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 02_CustomHtmlHelpers2/Extensions/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_CustomHtmlHelpers2.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ClearTurkishCharacters(this string text)
+        {
+            string turkish = "çış";
+            string english = "cis";
+
+            string result = string.Empty;
+
+            foreach (char item in text)
+            {
+                if (turkish.Contains(item))
+                {
+                    int index = turkish.IndexOf(item);
+                    result += english[index];
+                }
+                else
+                {
+                    result += item;
+                }
+            }
+            return result;
+        }
+    }
+}
